b6370de0b38507ad5d5b73e93804ef50a30624f0
Ernad Husremovic <hernad@bring.out.ba>
2011-02-27 21:47:53 +0100

    /report/racun/broj_racuna radi

--- seminarski/py/pos_db.py
+++ seminarski/py/pos_db.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python
 
+# author hernad@bring.out.ba
+# licenca: GPLv2: http://www.gnu.org/licenses/gpl-2.0.html
+
 import sqlite3
 import os.path
 from datetime import date
 
 
-VER="0.5.0"
+VER="0.9.1"
 DATE="26.02.2011"
 APP="UBP POS app"
 AUTHOR="hernad@bring.out.ba"
+
+
+PDV_STOPA=17
  
 def header():
    print "=" * 80
@@ -48,6 +54,7 @@ class Pos:
           print "vec postoji operater", naziv
         else:
          self.cursor.execute("insert into operateri(naziv) values(:naziv)", {'naziv': naziv})
+
 
      def parametar(self, svrha, opis):
 
@@ -107,23 +114,86 @@ class Pos:
         except sqlite3.Error, e:
             print "Desila se greska >>>>>>>>>>", e.args[0]
      
+     def get_next_broj_racuna(self, datum):
+        # nadji najveci za ovaj datum pa dodaj +1
+        print "datum=", datum 
+        ret = self.cursor.execute("select max(broj) from racuni where datum=:datum", {'datum': datum} ).fetchone()[0]
+        if not ret: 
+           print "na datum ", datum, "nema racuna, krecemo od 1"
+           return 1
+        else:
+           print "max broj racuna je", ret, " za datum ", date 
+           return ret+1
 
+     # ------------------ find sekcija -------------------------------------------------------
+
+     def find_parametar_by_svrha(self, svrha):
+        c_op = self.cursor.execute("select opis from parametri where svrha=:svrha", { 'svrha': svrha })
+        try:
+          return c_op.fetchone()[0]
+        except:
+          print "nepostoji parametar sa svrhom:", svrha
+          return None        
+  
+     def find_operater_by_naziv(self, naziv):
+        c_op = self.cursor.execute("select id from operateri where naziv = :naziv", {'naziv': naziv} )
+        try:
+          return c_op.fetchone()[0]
+        except:
+          print "nepostoji operater imena:", naziv
+          return None
+
+     def find_artikal_by_kod(self, kod):
+         row_artikal = self.cursor.execute("select id, cijena from artikli where kod=:kod", {'kod': kod}).fetchone()
+         if row_artikal:
+           return { 'id': row_artikal[0], 'cijena': row_artikal[1] }
+         else: 
+           return None 
+
+     def find_racun_by_id(self, id):
+         c_op = self.cursor.execute("select broj, datum, tip, ukupno_s_pdv from racuni where id=:id", {'id': id})
+         try:
+           tpl = c_op.fetchone()
+           return { 'broj': tpl[0], 'datum': tpl[1], 'tip': tpl[2], 'ukupno_s_pdv': tpl[3] }
+         except:
+           print "nepostoji racun id-a:", id
+           return None
+
+     def find_racun_by_broj_datum(self, broj, datum):
+         c_op = self.cursor.execute("select id, tip, ukupno_s_pdv from racuni where broj=:broj and datum=:datum", {'broj': broj, 'datum': datum})
+         try:
+           tpl = c_op.fetchone()
+           return { 'id': tpl[0], 'tip': tpl[1], 'ukupno_s_pdv': tpl[2] }
+         except:
+           print "nepostoji racun sa broj, datum:", broj, datum
+           return None
+
+     # --- kraj find sekcije -------------------------------------------------------------------------------
+                
      def add_racun(self, racun, rn_stavke):
-         r_operater = self.cursor.execute("select id from operateri where naziv = :operater", racun)
-          
-         racun['operater_id'] = r_operater.fetchone()[0]
-         print racun['operater_id']
-         self.cursor.execute("insert into racuni(tip, broj, datum, operater_id, ukupno_s_pdv) values(:tip, :broj, :datum, :operater_id, 0)", racun)
-         r_racun = self.cursor.execute("select id from racuni where broj = :broj and datum = :datum", racun)
-         racun['id'] = r_racun.fetchone()[0]
-         print "racun", r_racun, r_racun.fetchone()
-         for rn_stavka in rn_stavke:
-           row_artikal = self.cursor.execute("select id, cijena from artikli where kod=:artikal_kod", rn_stavka).fetchone()
+        # nije proslijedjen broj racuna, automatski ga odredi 
+        if not 'broj' in racun:
+          racun['broj'] = self.get_next_broj_racuna(racun['datum'])
+
+        racun['operater_id'] = self.find_operater_by_naziv(racun['operater'])
+        print racun['operater_id']
+        self.cursor.execute("insert into racuni(tip, broj, datum, operater_id, ukupno_s_pdv) values(:tip, :broj, :datum, :operater_id, 0)", racun)
+        r_racun = self.cursor.execute("select id from racuni where broj = :broj and datum = :datum", racun)
+        racun['id'] = r_racun.fetchone()[0]
+        print "racun", r_racun, r_racun.fetchone()
+        for rn_stavka in rn_stavke:
+           d_artikal = self.find_artikal_by_kod( rn_stavka['artikal_kod'] )
            rn_stavka['racun_id'] = racun['id']
-           rn_stavka['artikal_id'] = row_artikal[0]
-           rn_stavka['cijena'] = row_artikal[1]    
+           rn_stavka['artikal_id'] = d_artikal['id']
+           rn_stavka['cijena'] = d_artikal['cijena']    
            self.cursor.execute("insert into rn_stavke(racun_id, artikal_id, kolicina, cijena) values(:racun_id, :artikal_id, :kolicina, :cijena)", rn_stavka)
+        self.connection.commit()
+        return {'id': racun['id'], 'broj': racun['broj'], 'datum': racun['datum'] }
  
+      
+
+
+
      def __del__(self):
          self.connection.commit()
          print "bye bye pos object ..."
@@ -148,18 +218,88 @@ class PosPrint:
         for row in result:
            print row[0], row[1]
 
-    def artikli(self):
+    def artikli(self, to_str=None):
         result = self.pos.cursor.execute("select kod, naziv, jmj, barkod, cijena from artikli")
+        out = ""
         # izlistaj parametre 
         for row in result:
-           #try:
-           #http://docs.python.org/library/string.html
-           print '{kod:<10} - {naziv:<40} ({jmj:<3}) : barkod: {barkod:<13}, cijena = {cijena:8.2f} '.format(kod=row[0], naziv=row[1], jmj=row[2], barkod=row[3], cijena=row[4])
-           #except:
-           #   print "ne mogu odstampati", row[0], "-", row[1] 
+           str = '{kod:<10} - {naziv:<40} ({jmj:<3}) : barkod: {barkod:<13}, cijena = {cijena:8.2f} '.format(kod=row[0], naziv=row[1], jmj=row[2], barkod=row[3], cijena=row[4])
+           if to_str:
+              out += str + '\n'
+           else:
+              print str
 
+        if to_str:
+           return out
 
+    def racun(self, id):
+       rn = self.pos.find_racun_by_id(id)
+       if not rn:
+          return "nema racuna id={0}".format(id) 
+       header = {
+          'naziv_firme': self.pos.find_parametar_by_svrha("F"),
+          'pdv_broj':  self.pos.find_parametar_by_svrha("P"),
+          'naziv_p': self.pos.find_parametar_by_svrha("N"),
+          'adresa_p': self.pos.find_parametar_by_svrha("A"),
+          'telefon': self.pos.find_parametar_by_svrha("T"),
+          'fax': self.pos.find_parametar_by_svrha("U"),
+          'broj': rn['broj'],
+          'datum': rn['datum'],
+          'tip': rn['tip']
+       }
 
+       ciza = "-" * 35
+       if rn['tip'] == 1:
+           tip_rn = "REDOVAN"
+       else:
+           tip_rn = "REKLAMIRANI"
+       s_out = """{naziv_firme}
+PDV Broj: {pdv_broj}
+{naziv_p}
+{adresa_p}
+tel: {telefon}
+fax: {fax}
+{tip_rn} Racun br. {broj}
+Sarajevo, {datum}
+
+{ciza}
+R.br Sifra       Naziv(jmj)
+              kol.    cij. ukupno
+{ciza}\n""".format(naziv_firme=header['naziv_firme'], pdv_broj=header['pdv_broj'], naziv_p=header['naziv_p'], adresa_p=header['adresa_p'], telefon=header['telefon'], fax=header['fax'], broj=header['broj'], datum=header['datum'], ciza=ciza, tip_rn=tip_rn)       
+        
+
+       c_stavke = self.pos.cursor.execute("select artikli.kod, artikli.naziv, artikli.jmj, artikli.barkod, rn_stavke.cijena, rn_stavke.kolicina from rn_stavke LEFT OUTER JOIN artikli ON rn_stavke.artikal_id = artikli.id WHERE racun_id=:id ", {'id': id})
+ 
+       rbr = 0
+       ukupno_t = 0
+       for st in c_stavke:
+           rbr += 1
+           artikal_kod = st[0]
+           naziv_art = st[1] + '(' + st[2] + ')' 
+           barkod =st[3] 
+           cijena = st[4]
+           kolicina = st[5]
+           ukupno = kolicina * cijena
+           ukupno_t += ukupno
+           s_out = s_out + """{rbr:3}. {naziv_art:20}
+{kod:10} {kolicina:6} {cijena:7} {ukupno:9}\n""".format(rbr=rbr, naziv_art=naziv_art, kolicina=kolicina, cijena=cijena, ukupno=ukupno, kod=artikal_kod)
+
+       ukupno_bez_pdv_t = ukupno_t / ((100.00 + PDV_STOPA)/100.00)
+       ukupno_pdv_t = ukupno_bez_pdv_t * (PDV_STOPA/100.00)
+       # ukupno sa pdv ne smijemo dirati zato cemo gresku u zaokruzenju prebaciti na ukupno bez pdv
+       ukupno_pdv_t = round(ukupno_pdv_t, 2)
+       ukupno_t = round(ukupno_t, 2)
+       ukupno_bez_pdv_t = ukupno_t - ukupno_pdv_t
+
+       s_out = s_out + """{ciza}
+Ukupno bez PDV         {uk_bez:12}
+iznos   PDV            {uk_pdv:12}
+{ciza}
+Ukupno sa PDV          {uk_s_pdv:12}
+{ciza}""".format(uk_bez=ukupno_bez_pdv_t, uk_pdv=ukupno_pdv_t, uk_s_pdv=ukupno_t, ciza=ciza)
+       print s_out
+       return s_out
+ 
 
 if __name__ == '__main__':
 
@@ -213,9 +353,9 @@ if __name__ == '__main__':
     print("-" * 80)
     print_pos.artikli()
 
-    for i in range(1, 11):
-      racun = { 'tip':1, 'broj':i, 'datum': date.today(), 'operater': 'hernad' }
-      rn_stavke = [ { 'artikal_kod': 'POS', 'kolicina': 1+i },
+    for i in range(1, 0):
+      racun = { 'tip':1, 'datum': date.today(), 'operater': 'hernad' }
+      rn_stavke = [ { 'artikal_kod': 'USB5', 'kolicina': 1+i },
                     { 'artikal_kod': 'SP9', 'kolicina': 2+i }
                   ]
       p.add_racun(racun, rn_stavke)
@@ -227,5 +367,8 @@ if __name__ == '__main__':
     #print (result.fetchall())
     #p2 = Pos("ubp_pos_2.db")
     #print p2.cursor
+
+    print_pos.racun(5)
+
 
 
--- seminarski/py/pos_web_server.py
+++ seminarski/py/pos_web_server.py
@@ -1,74 +1,326 @@
+#!/usr/bin/python
+
+# author hernad@bring.out.ba
+# licenca: AGPL: http://www.gnu.org/licenses/agpl.html
+
 import StringIO
 import datetime
 import pos_db
-
+import cgi
+from datetime import date
 
 from wsgiref.simple_server import make_server
 from urllib import unquote_plus
 
-
 PORT=3000
+VER="0.5.0"
+AUTHOR="hernad@bring.out.ba"
+
+class PosWeb:
+
+   pos = None
+   port = 0
+   # racun { tip, operater, datum }
+   racun = {}
+   # rn_stavka - dict { artikal_kod, kolicina }
+   rn_stavke = []
+   err_msg = None
+   # napravljen je ovaj racun
+   print_rn_id = None
 
 
+   def __init__(self, port):
+      self.port = port
+      self.pos = pos_db.Pos("ubp_pos.db")
 
-def get_form_vals(post_str):
+   def get_form_vals(self, post_str):
 
-   print "-" * 80
-   print "post_str=", post_str
-   print "-" * 80
-   form_vals = {}
-   for item in post_str.split("&"):
-      form_vals[item.split("=")[0]] = item.split("=")[1]
+      print "-" * 80
+      print "post_str=", post_str
+      print "-" * 80
+      form_vals = {}
+      for item in post_str.split("&"):
+         form_vals[item.split("=")[0]] = item.split("=")[1]
 
-   print form_vals
-   return form_vals
+      print form_vals
+      return form_vals
+
+   def html_start(self, output):
+      output.write("""
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+        """)
+
+   def html_head_start(self, output):
+      output.write("""<head>""")
+       
+   def html_head_end(self, output):
+      output.write("""</head>
+      <body>""")
+
+   def html_footer(self, output):
+      # http://wiki.python.org/moin/EscapingHtml
+      output.write("<p/></p>" + cgi.escape(AUTHOR + ", ver: " + VER))
 
 
-def pos_web_app(environ, response):
-   #from io import StringIO
-   output = StringIO.StringIO()
+   def html_rn_stavke(self, output):
+       print >>output, "<table>\n"
+       tbl_row = "<tr><td>{0}</td><td>{1}</td></tr>\n"
+       
+       print >>output, tbl_row.format("-------", "------------")
+       print >>output, tbl_row.format("artikal", "kolicina")
+       for stavka in self.rn_stavke:
+           print >>output, tbl_row.format(stavka['artikal_kod'], stavka['kolicina'])
+       print >>output, tbl_row.format("-------", "------------")
+
+       print >>output, "</table>\n"
+       
+   def html_error(self, output):
+       if self.err_msg:
+          print self.err_msg
+          output.write("<b>{0}</b><p/><p/>".format(cgi.escape(self.err_msg)))
+       
+          if self.print_rn_id:
+             #odstampaj racun
+             pos_print = pos_db.PosPrint(self.pos)
+             str_racun = cgi.escape(pos_print.racun(self.print_rn_id))
+             output.write("<pre>{0}</pre><p>".format(str_racun) )
+             self.print_rn_id = None
+          else:
+             #odstampaj artikle
+             output.write("Lista postojecih artikala:<p><p>")
+             pos_print = pos_db.PosPrint(self.pos)
+             str_artikli = cgi.escape(pos_print.artikli(True))
+             output.write("<pre>{0}</pre><p>".format(str_artikli) )
+          self.err_msg = None
+
+   def html_end(self, output):
+      output.write("""
+        </body>
+        </html>""")
+
+   def html_form_racun(self, output):
+      # http://stackoverflow.com/questions/991922/how-does-a-html-form-identify-itself-in-post-header
+      # http://www.htmlcodetutorial.com/forms/_INPUT_TYPE_RADIO.html
+
+      # setuj button na osnovu stanja racun['tip']
+      if 'tip' in self.racun:
+         if self.racun['tip'] == 1:
+            s_0 = 'checked'
+            s_1 = ''
+         else:
+            s_0 = ''
+            s_1 = 'checked'
+      else:
+         s_0 = 'checked'
+         s_1 = ''
+
+      if 'operater' in self.racun:
+          s_op = '"' + self.racun['operater'] + '"'
+      else:
+          s_op = '""'
+
+      print >>output,  """<form method="POST" onsubmit="return true" id="zakljuci_rn" >
+           Tip racuna:
+            <INPUT TYPE=RADIO NAME="tip" VALUE="1" {0}>redovni
+            <INPUT TYPE=RADIO NAME="tip" VALUE="2"  {1}>reklamirani
+           operater: <input type="text" name="operater" id="operater" value={2} />
+           <input type="submit" value="Zakljuci racun" name="form_racun" /> 
+           </form>""".format(s_0, s_1, s_op)
 
 
-   # HTTP ok
-   status = b'200 OK'
-   # HTTP header
-   headers = [ (b'Content-type', b'text/html; charset=utf-8') ]
+   def html_form_artikli(self, output):
+      print >>output,  """<form method="POST" onsubmit="return validate_unos()" id="dodaj_stavku" >
+           Artikal: <input type="text" name="artikal" id="artikal" />
+           kolicina: <input type="text" name="kolicina" id="kolicina" /> 
+           <input type="submit" value="Dodaj stavku" name="form_stavke" />
+           </form>"""
+
+
+   def html_report_racun(self, output, broj):
+        #odstampaj racun
+        print "report 1 / stampa danasnjeg racuna broj:", broj
+        pos_print = pos_db.PosPrint(self.pos)
+        rn = self.pos.find_racun_by_broj_datum(broj, date.today())
+        str_racun = cgi.escape(pos_print.racun(rn['id']))
+        output.write("<pre>{0}</pre><p>".format(str_racun) )
+
+
+   # javascript validation
+   def js_validate(self, output):
+
+      output.write("""
+      <script type="text/javascript">
+      function validate_fields() {
+         artikal = document.getElementById('artikal');
+         artikal = artikal.value;
+
+         kolicina = document.getElementById('kolicina');
+         kolicina = kolicina.value;
+
+         ret = true;
+         err = '';
+
+         //alert('artikal =" + artikal);
+         if( artikal.length == 0 ) {
+            err = err + ' (1) artikal ne moze biti prazan !';
+            ret = false;
+         } 
+
+         if( kolicina.length == 0 ) {
+            err = err + ' (2) polje kolicina ne moze biti prazno !';
+            ret = false;
+         } 
+
+         var reNum = new RegExp('[a-zA-Z]+', 'mig');
+         if (reNum.test(kolicina)) {
+           err = err + " (3) kolicina mora biti broj (formata 999.99) !";
+           ret = false;
+         }
+
+         if (!ret)
+             alert(err);
+
+         return ret;
+      }
+        
+      function validate_unos() {
+          ret = true;
+          ret = ret && validate_fields();
+          //if (!ret) {
+          //  alert("validacija neuspjesna!");
+          //}
+          return ret;
+      }
+
+      </script>
+        """)
+    
+   def process(self, environ, response):
+      
+      output = StringIO.StringIO()
+      # HTTP ok
+      status = b'200 OK'
+      # HTTP header
+      headers = [ (b'Content-type', b'text/html; charset=utf-8') ]
    
-   response(status, headers)
+      response(status, headers)
 
-   print >>output, "<h1>UBP seminarski POS app web klijent</h1>"
+      self.html_start(output)
+      self.html_head_start(output)
+      self.js_validate(output)
+      self.html_head_end(output)
 
-   if environ['REQUEST_METHOD'] == 'POST':
-      process_post(environ)
+      print >>output, "<h1>UBP seminarski POS app web klijent</h1>"
 
-   path_vals = environ['PATH_INFO'][1:].split("/")
+      path_vals = environ['PATH_INFO'][1:].split("/")
+      print path_vals
 
-   #user, *tag = path_vals
-   print "path_vals=", path_vals
-   #print "user=", user
-   #print "tag=", tag
+      
+      if path_vals[0] == 'report' and path_vals[1] and path_vals[2]:
+         print "procesiram report ..."
+         process_rpt = True
+         # report/1/broj_danasnjeg_racuna
+         if path_vals[1] == "racun" and int(path_vals[2]) > 0:
+            # stampa racuna na danasnji dan
+            self.html_report_racun(output, int(path_vals[2]))
+      else:
+         process_rpt = False
+               
+
+      
+      if environ['REQUEST_METHOD'] == 'POST':
+         self.process_post(environ)
+
+      if not process_rpt:
+        self.html_form_racun(output)
+
+        if not self.err_msg:
+           # prikazi do sada unesene stavke
+           self.html_rn_stavke(output)
+        self.html_error(output)
+        self.html_form_artikli(output)
+
+      self.html_footer(output)
+      self.html_end(output)
+
+      return [output.getvalue()]
+
+   def set_err_msg(self, err_msg):
+       self.err_msg = err_msg
+
+   # process HTTP POST 
+   def process_post(self, environ):
+      size = int(environ['CONTENT_LENGTH'])
+      #print environ
+      post_str = unquote_plus(environ['wsgi.input'].read(size).decode())
    
-   print >>output, """<form method="POST">
-         Artikal: <input type="text" name="artikal">
-         kolicina: <input type="text" name="kolicina"> 
-         <input type="submit" value="Dodaj stavku">
-         </form>"""
+      form_vals = self.get_form_vals(post_str)
+      print form_vals
 
-   return [output.getvalue()]
+      if 'form_racun' in form_vals:
+          self.process_racun(form_vals)
+      elif 'form_stavke' in form_vals:
+          self.process_stavke(form_vals)
+      else:
+          print "KAKVA JE OVO FORMA ?!??"
 
-   
-def process_post(environ):
-   size = int(environ['CONTENT_LENGTH'])
-   post_str = unquote_plus(environ['wsgi.input'].read(size).decode())
-   
-   form_vals = get_form_vals(post_str)
-   form_vals['timestamp'] = datetime.datetime.now()
+   def zakljuci_racun(self):
+      print "zakljucujem racun"
+      self.racun['datum'] = date.today()
+      rn = self.pos.add_racun(self.racun, self.rn_stavke)
+      if rn['broj'] > 0:
+         self.set_err_msg("Racun br. {0} datuma {1} AZURIRAN USPJESNO !".format(rn['broj'], rn['datum']))
+         # treba ovaj racun odstampati
+         self.print_rn_id = rn['id']
 
-   print form_vals
+         # neka zapamti operatera, ostalo neka resetuje
+         op = self.racun['operater']
+         self.racun = { 'operater': op }
+         
+         self.rn_stavke = []
 
-   
-print("Server slusa na portu %s" % (PORT))
-httpd = make_server('', PORT, pos_web_app)
-httpd.serve_forever()
 
+   def process_racun(self, form_vals):
+      if form_vals['tip'] == "1":
+          print "redovni racun"
+          self.racun['tip'] = 1
+      else:
+          print "reklamirani racun"
+          self.racun['tip'] = 2
+
+      operater = self.pos.find_operater_by_naziv( form_vals['operater'])
+      if not operater:
+         self.set_err_msg("Operater " + form_vals['operater'] + " nepoznat !")
+         return
+      else:
+         self.racun['operater'] = form_vals['operater']
+
+      if len(self.rn_stavke) < 1:
+         self.set_err_msg("Racun ne moze biti zakljucen ako nema stavki")
+      else:
+         self.zakljuci_racun()
+         
+   def process_stavke(self, form_vals):
+      form_vals['timestamp'] = datetime.datetime.now()
+
+      if form_vals['artikal']:
+         d_artikal = self.pos.find_artikal_by_kod( form_vals['artikal'] )
+         if d_artikal:
+           self.rn_stavke.append( {'artikal_kod': form_vals['artikal'], 'kolicina':form_vals['kolicina']} )
+         else:
+           self.set_err_msg("Artikal kod: '" + form_vals['artikal'] + "' je nepostojeci !")
+
+      print self.rn_stavke
+
+
+
+
+   def serve(self):
+      print("Server slusa na portu %s" % (self.port))
+      httpd = make_server('', self.port, self.process)
+      httpd.serve_forever()
+
+
+pos_web = PosWeb(PORT)
+pos_web.serve()
 

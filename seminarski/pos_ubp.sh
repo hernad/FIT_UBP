967d13907f1a233ebc066fecf683137b6f20ee9e
Ernad Husremovic <hernad@bring.out.ba>
2011-03-03 13:45:00 +0100

    0.7.0 E-R diagram

--- seminarski/pos_ubp.sh
+++ seminarski/pos_ubp.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-erviz.sh -i pos_ubp.txt -o pos_ubp.dot
-work/scripts/dot2pdf.sh pos_ubp.dot
+export ERVIZ_BASE=/home/bringout/devel/erviz/erviz-1.0.6
 
+export PATH=$PATH:$ERVIZ_BASE/bin
+
+erviz.sh -i pos_ubp.txt -o pos_ubp.dot
+
+THIS_DIR=`pwd`
+echo `pwd`
+cd $ERVIZ_BASE
+
+echo `pwd`
+work/scripts/dot2pdf.sh $THIS_DIR/pos_ubp.dot
+work/scripts/dot2png.sh $THIS_DIR/pos_ubp.dot
 
